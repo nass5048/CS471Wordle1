b6c915dfa243aa9522f105d98324a1063890cf33
Mike Nassoiy <nass5048@kettering.edu>
2026-04-20 18:34:27 -0400

    added json db solution needs to be a little better such as onyl serving from backend

---
+++ WordleAPI/CustomDb.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace WordleAPI
+{
+    public class CustomDb<TItem>
+    {
+        string FilePath { get; set; }
+        string StoredString { get; set; } = "";
+        public List<TItem> Item { get; set; } = new();
+        public CustomDb(string filePath)
+        {
+            FilePath = filePath;
+            try
+            {
+                StoredString = File.ReadAllText(FilePath);
+                //retrieve from file so we dont have to setup DB 
+                Item = JsonSerializer.Deserialize<List<TItem>>(StoredString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading from file: {ex.Message}");
+                Item = new List<TItem>();
+            }
+        }
+        public bool SaveToDB()
+        {
+            StoredString = JsonSerializer.Serialize(Item);
+            File.WriteAllText(FilePath, StoredString);
+            Console.WriteLine($"Error reaasdqdage");
+            return true;
+        }
+    }
+}
