aa653797c8cbf9838688c2a1e4f20972336fd804
Mike Nassoiy <nass5048@kettering.edu>
2026-04-30 13:44:01 -0400

    added frontend

---
+++ CS471Wordle1/Services/SpinnerService.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Services;
+
+public class SpinnerService
+{
+    public bool IsShowing { get; private set; }
+
+    // Event with current visibility state
+    public event Action<bool>? OnChange;
+
+    public void Show() => Toggle(true);
+    public void Hide() => Toggle(false);
+
+    public void Toggle(bool show)
+    {
+        if (IsShowing == show) return;
+        IsShowing = show;
+        OnChange?.Invoke(IsShowing);
+    }
+}
