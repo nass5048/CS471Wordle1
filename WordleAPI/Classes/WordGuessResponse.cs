46f5fab25c9b2ef2d30d04bb558c9973d167b9b3
Mike Nassoiy <nass5048@kettering.edu>
2026-05-04 02:43:17 -0400

    added the different challange modes

--- WordleAPI/Classes/WordGuessResponse.cs
+++ WordleAPI/Classes/WordGuessResponse.cs
@@ -42,8 +42,28 @@ public class WordGuessResponse
                     }
                     break;
                 case Challenge.AllYellow:
+                    if (CorrectWord == GuessedWord)
+                    {
+                        Colors.Add(ResponseColor.green);
+                    }
+                    else if (ShouldYellowOnly(i, GuessedWord, CorrectWord))
+                    {
+                        Colors.Add(ResponseColor.yellow);
+                    }
+                    else
+                    {
+                        Colors.Add(ResponseColor.grey);
+                    }
                     break;
                 case Challenge.AllGreen:
+                    if (CorrectWord[i] == GuessedWord[i])
+                    {
+                        Colors.Add(ResponseColor.green);
+                    }
+                    else
+                    {
+                        Colors.Add(ResponseColor.grey);
+                    }
                     break;
             }
         }
@@ -61,6 +81,45 @@ public class WordGuessResponse
             if (guess[i] == correct[i] && guess[index] == guess[i])
                 greenLetterCount++;
         }
+        //if there are the exact amount of green letters as number of letters than you have all the correct letters no letters should be yellow
+        if (greenLetterCount == correctLetterCheck)
+        {
+            return false;
+        }
+        //check number of letters in guess up to index
+        int letterCheck = 0;
+        for (int i = 0; i <= index; i++)
+        {
+            if (guess[i] == guess[index])
+                letterCheck++;
+        }
+        //if there are more green letters than number of letters than you know the rezt have to be grey
+        if (letterCheck > correctLetterCheck)
+        {
+            return false;
+        }
+
+        if (correct.Contains(guess[index]))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool ShouldYellowOnly(int index, string guess, string correct)
+    {
+        if(guess == correct)
+            return false;
+
+        int correctLetterCheck = correct.Where(p => p == guess[index]).Count();
+        //gets if the letters can be yellow or grey
+        //check numbe rof green letters
+        int greenLetterCount = 0;
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    if (guess[i] == correct[i] && guess[index] == guess[i])
+        //        greenLetterCount++;
+        //}
         //if there are the exact amount of green letters as number of letters than you have all the correct letters no letters should be yellow
         if (greenLetterCount == correctLetterCheck)
         {
