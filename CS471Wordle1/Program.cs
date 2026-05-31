9bda66c02e9d0c8b2962dbad228f47d20f64a136
Mike Nassoiy <nass5048@kettering.edu>
2026-05-31 15:54:20 -0400

    persist Login until logout

--- CS471Wordle1/Program.cs
+++ CS471Wordle1/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WordleAPI;
 using WordleAPI.Classes;
+using WordleBackend;
 using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,5 +14,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddSingleton<Services.SpinnerService>();
 builder.Services.AddSingleton<DataBase>();
 builder.Services.AddSingleton<Login>();
+builder.Services.AddScoped<BrowserStorage>();
 
 await builder.Build().RunAsync();
---
+++ WordleAPI/BrowserStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace WordleBackend
+{
+    public class BrowserStorage
+    {
+        private readonly IJSRuntime js;
+
+        public BrowserStorage(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        public ValueTask Set(string key, string value) =>
+            js.InvokeVoidAsync("localStorage.setItem", key, value);
+
+        public ValueTask<string> Get(string key) =>
+            js.InvokeAsync<string>("localStorage.getItem", key);
+
+        public ValueTask Remove(string key) =>
+            js.InvokeVoidAsync("localStorage.removeItem", key);
+    }
+}
