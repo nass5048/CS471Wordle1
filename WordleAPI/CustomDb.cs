9e77e28aa78e18434c82b1a440bf85ae80b29e6e
Jeanna Laureta <laur2545@kettering.edu>
2026-06-02 16:39:34 -0400

    some things

--- WordleAPI/CustomDb.cs
+++ WordleAPI/CustomDb.cs
@@ -38,13 +38,13 @@ public class DataBase
 
     public async Task LoadData(HttpClient Http)
     {
-        var test = await Http.GetFromJsonAsync<DataBase>("https://localhost:7160/api/data");
+        var test = await Http.GetFromJsonAsync<DataBase>("http://localhost:7160/api/data");
         if (test != null)
             UpdateStoredDB(test);
     }
     public async Task SaveData(HttpClient Http)
     {
-        await Http.PostAsJsonAsync("https://localhost:7160/api/data", this);
+        await Http.PostAsJsonAsync("http://localhost:7160/api/data", this);
     }
 
     public void UpdateUserData(Login login)
