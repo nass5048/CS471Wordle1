b0fe8b0125dfec37a0a2c975422dea6a19fd95e6
Jeanna Laureta <laur2545@kettering.edu>
2026-06-03 00:13:46 -0400

    Added comments to CustomDB.cs

--- WordleAPI/CustomDb.cs
+++ WordleAPI/CustomDb.cs
@@ -6,19 +6,25 @@ using WordleAPI.Classes;
 using WordleBackend.Classes;
 
 namespace WordleAPI;
+
 public class DataBase
 {
+    // Lists that store main data 
     public List<DailyWords> DailyWords { get; set; }
-
-
     //public List<Stats?> Stats { get; set; }
     public List<Login> Login { get; set; }
-
     public List<FriendRequest> FriendRequests { get; set; }
-
     public List<BugReport> BugReports { get; set; }
 
     // Optionally pass a basePath from the host (for example IWebHostEnvironment.ContentRootPath)
+    /*
+        Function Name: DataBase
+        Input: N/A
+        Output: Database object
+        Brief Description:
+            Initializes lists to store data
+    */
+
     public DataBase()
     {
         //Stats = new List<Stats?>();
@@ -27,6 +33,15 @@ public class DataBase
         FriendRequests = new List<FriendRequest>();
         BugReports = new List<BugReport>();
     }
+
+    /*
+        Function Name: UpdateStoredDB
+        Input: Database object
+        Output: N/A
+        Brief Description:
+            Replace data stored with data from database
+    */
+
     public void UpdateStoredDB(DataBase dataBase)
     {
         //Stats = dataBase.Stats;
@@ -36,16 +51,41 @@ public class DataBase
         BugReports = dataBase.BugReports;
     }
 
+    /*
+        Function Name: LoadData
+        Input: HTTP client that requests data from API
+        Output: N/A
+        Brief Description:
+            Loads current database info from the backend API and updates the local storage
+    */
+
     public async Task LoadData(HttpClient Http)
     {
         var test = await Http.GetFromJsonAsync<DataBase>("http://localhost:7160/api/data");
         if (test != null)
             UpdateStoredDB(test);
     }
+
+    /*
+        Function Name: SaveData
+        Input: HTTP client that saves data from API
+        Output: N/A
+        Brief Description:
+            Sends current database info to the backend API for storage
+    */
+
     public async Task SaveData(HttpClient Http)
     {
         await Http.PostAsJsonAsync("http://localhost:7160/api/data", this);
     }
+
+    /*
+        Function Name: UpdateUserData
+        Input: Updated login info for the user that requests the change
+        Output: N/A
+        Brief Description:
+            Finds user via UserID and updates saved login/account infor
+    */
 
     public void UpdateUserData(Login login)
     {
