fcd6748a4f4ac93569693e8ed964488d57706de4
Jeanna Laureta <laur2545@kettering.edu>
2026-06-04 16:01:56 -0400

    Added comments to SpinnerService.cs and Program.cs in CS471Wordle1 folder

--- CS471Wordle1/Program.cs
+++ CS471Wordle1/Program.cs
@@ -6,14 +6,24 @@ using WordleAPI.Classes;
 using WordleBackend;
 using static System.Net.Mime.MediaTypeNames;
 
+
+// Create and configure the Blazor WebAssembly app
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Register root app components
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Register services and app data
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Shared services and app data
 builder.Services.AddSingleton<Services.SpinnerService>();
 builder.Services.AddSingleton<DataBase>();
 builder.Services.AddSingleton<Login>();
+
+// Browser local storage helper service
 builder.Services.AddScoped<BrowserStorage>();
 
+// Build and launch app
 await builder.Build().RunAsync();
--- CS471Wordle1/Services/SpinnerService.cs
+++ CS471Wordle1/Services/SpinnerService.cs
@@ -4,13 +4,39 @@ namespace Services;
 
 public class SpinnerService
 {
+    // Tracks whether the loading spinner is currently visible
     public bool IsShowing { get; private set; }
 
     // Event with current visibility state
     public event Action<bool>? OnChange;
 
+    /*
+        Function Name: Show
+        Input: N/A
+        Output: N/A
+        Brief description:
+            Displays spinner
+    */
+
     public void Show() => Toggle(true);
+
+    /*
+        Function Name: Hide
+        Input: N/A
+        Output: N/A
+        Brief description:
+            Hides spinner
+    */
+    
     public void Hide() => Toggle(false);
+
+    /*
+        Function Name: Toggle 
+        Input: true (should be shown) or false (should be hidden)
+        Output: N/A
+        Brief description:
+            Updates spinner visbility state
+    */    
 
     public void Toggle(bool show)
     {
